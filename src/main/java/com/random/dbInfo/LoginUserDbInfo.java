fbc073fba5565a80a2cc0d1ef77e1cb6648c55ba
ouioui99 <qp5minute@gmail.com>
2022-04-20 10:37:31 +0900

    細かい修正

--- src/main/java/com/random/dbInfo/LoginUserDbInfo.java
+++ src/main/java/com/random/dbInfo/LoginUserDbInfo.java
@@ -1,9 +1,16 @@
 package com.random.dbInfo;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class LoginUserDbInfo {
+public class LoginUserDbInfo implements Serializable{
+
+    // serialVersionUIDを指定
+    private static final long serialVersionUID = 1L;
+
+
     int id;
     String name;
 }
