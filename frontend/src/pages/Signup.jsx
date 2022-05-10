6e25cf4e93a90ccf48a485e4b3c13e9380cb0166
ouioui99 <qp5minute@gmail.com>
2022-05-10 13:26:38 +0900

    signup from 整形

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,4 +1,12 @@
 import React, { useState,useContext }from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import { UserContext } from '../providers/UserProvider';
 import {postSignup} from '../api/userAxios';
 import { useNavigate } from 'react-router-dom';
@@ -14,6 +22,8 @@ export const Signup = () => {
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
     const [passwordConf, setPasswordConf] = useState("");
+
+    const theme = createTheme();
 
     const handleSubmit = (e) => {
         if (password === passwordConf) {
@@ -36,17 +46,72 @@ export const Signup = () => {
     return (
         <>
             <Header></Header>
-            <h1>Signup</h1>
-            <form  onSubmit={(e) => handleSubmit(e)}>
-                <ul>
-                    <li><input type="text" name="name" value={name} required onChange={(e) => setName(e.target.value)}></input></li>
-                    <li><input type="text" name="password" value={password} required onChange={(e) => setPassword(e.target.value)}></input></li>
-                    <li><input type="text" name="passwordConf" value={passwordConf} required onChange={(e) => setPasswordConf(e.target.value)}></input></li>
-                    <li><input type="submit" value="Submit"></input></li>
-                </ul>
-            </form>
-            <h3>name:{name}</h3>
-            
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                    >
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                        <Box component="form" noValidate sx={{ mt: 1 }}>
+                            <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="name"
+                            label="name"
+                            name="name"
+                            autoComplete="name"
+                            autoFocus
+                            onChange={(e) => setName(e.target.value)}
+                            />
+                            <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="passwordConf"
+                            label="Password Confirm"
+                            type="password"
+                            id="passwordConf"
+                            autoComplete="current-password"
+                            onChange={(e) => setPasswordConf(e.target.value)}
+                            />
+                            {/* <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                            /> */}
+                            <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            disabled={!name || !password || !passwordConf ? true : false}
+                            onClick={(e)=>handleSubmit(e)}
+                            >
+                            Sign Up
+                            </Button>
+                        </Box>
+                    </Box>
+                </Container>
+            </ThemeProvider>
         </>
     )
 }
