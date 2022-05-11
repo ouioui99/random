c21d429c4fdbc5ccd4df46f916723c5a932e15e4
ouioui99 <qp5minute@gmail.com>
2022-05-11 10:46:49 +0900

    selectorをcomponent化して分離

--- frontend/src/pages/MainHome.jsx
+++ frontend/src/pages/MainHome.jsx
@@ -1,15 +1,31 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
 import { CheckLoggedIn } from "../CheckLogin";
 import { UserContext } from '../providers/UserProvider';
 import {getRestraunt} from '../api/getRestrauntAxios';
 import {MyComponent} from '../components/GoogleMap';
+import {GenreCodeSelector} from '../components/GenreCodeSelector';
+import {BudgetCodeSelector} from '../components/BudgetCodeSelector';
+
 
 import Header from "../components/materialUi/Header";
-import {GloabalButton} from '../components/materialUi/Button';
 
 
 export const MainHome = () => {
+
+    const theme = createTheme();
 
     useEffect(() => {
         setIsLoggedIn(CheckLoggedIn());
@@ -27,19 +43,17 @@ export const MainHome = () => {
     const [lng, setLng] = useState(139.77521);
     const [catchPhrase, setCatchPhrase] = useState("");
     const [genre, setGenre] = useState("");
+    const [searched, setSearched] = useState(false);
 
-    const navigate = useNavigate();
-
-    const GoogleApiKey = process.env.React_APP_GOOGLE_MAP_API;
-
-    const clicked = () => {
+    const clicked = (e) => {
+        e.preventDefault();
         getRestraunt({
             referenceSite:referenceSite,
             genreCode:genreCode,
             bugetCode:bugetCode,
         }).then((response) => {
             if(response.status===200) {
-                console.log("OK");
+                setSearched(true);
                 setName(response.data.name);
                 setAddress(response.data.address);
                 setUrl(response.data.url);
@@ -51,7 +65,6 @@ export const MainHome = () => {
                 alert("検索結果が見つかりませんでした");
             }
         })
-        // navigate('/test');
     }
 
 
@@ -59,53 +72,101 @@ export const MainHome = () => {
     return (
         <>
             <Header></Header>
-            <h1>MainHome</h1>
-            <input type="text" name="referenceSite" onChange={(e) => setReferenceSite(e.target.value)}></input><br />
-            <select name="genre" onChange={(e) => setGenreCode(e.target.value)}>
-                <option value="">--Please choose an option--</option>
-                <option value="G001">居酒屋</option>
-                <option value="G002">ダイニングバー・バル</option>
-                <option value="G003">創作料理</option>
-                <option value="G004">和食</option>
-                <option value="G005">洋食</option>
-                <option value="G006">イタリアン・フレンチ</option>
-                <option value="G007">中華</option>
-                <option value="G008">焼肉・ホルモン</option>
-                <option value="G017">韓国料理</option>
-                <option value="G009">アジア・エスニック料理</option>
-                <option value="G010">各国料理</option>
-                <option value="G011">カラオケ・パーティ</option>
-                <option value="G012">バー・カクテル</option>
-                <option value="G013">ラーメン</option>
-                <option value="G016">お好み焼き・もんじゃ</option>
-                <option value="G014">カフェ・スイーツ</option>
-                <option value="G015">その他グルメ</option>
-            </select>
-            <select name="budget" onChange={(e) => setBugetCode(e.target.value)}>
-                <option value="">--Please choose an option--</option>
-                <option value="B009,B010">~1000</option>
-                <option value="B011">1001~1500</option>
-                <option value="B001">1501~2000</option>
-                <option value="B002,B003">2001~4000</option>
-                <option value="B008,B004">4001~7000</option>
-                <option value="B005">7001~10000</option>
-                <option value="B006">10001~15000</option>
-                <option value="B012">15001~20000</option>
-                <option value="B013">20001~30000</option>
-                <option value="B014">30001~</option>
-            </select>
-            <button onClick={clicked}>検索</button>
-            <h3>{referenceSite}</h3>
-            
-            <h3>{address}</h3>
-            <a href={url}><h3>{name}</h3></a>
-            <h3>{lat}</h3>
-            <h3>{lng}</h3>
-            <h3>{catchPhrase}</h3>
-            <h3>{genre}</h3>
-            <MyComponent lat={lat} lng={lng}/>
+            <ThemeProvider theme={theme}>
+                <Grid container component="main" sx={{ height: '100vh' }}>
+                <CssBaseline />
+                    <Grid
+                        item
+                        xs={false}
+                        sm={4}
+                        md={7}
+                        sx={{
+                        backgroundColor: (t) =>
+                            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        }}
+                    >
+                        <MyComponent lat={lat} lng={lng}/>
+                    </Grid>
+
+                    <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                        <Box
+                            sx={{
+                                my: 8,
+                                mx: 4,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'center',
+                            }}
+                            >
+                            <Typography component="h1" variant="h5">
+                                飲食店ランダム検索
+                            </Typography>
+                            <Box component="form" noValidate sx={{ mt: 1 }}>
+
+                                {searched ? 
+                                <>
+                                    <h3>{address}</h3>
+                                    <a href={url}><h3>{name}</h3></a>
+                                    <h3>{lat}</h3>
+                                    <h3>{lng}</h3>
+                                    <h3>{catchPhrase}</h3>
+                                    <h3>{genre}</h3>
+                                    {/* size固定したい */}
+                                    <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                    onClick={(e)=>clicked(e)}
+                                    >
+                                    再検索
+                                    </Button>
+                                    {/* size固定したい */}
+                                    <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                    onClick={()=>setSearched(false)}
+                                    >
+                                    検索条件変更
+                                    </Button>
+                                </>:
+                                <>
+                                    <TextField
+                                    margin="normal"
+                                    required
+                                    fullWidth
+                                    id="referenceSite"
+                                    label="基準地"
+                                    name="referenceSite"
+                                    autoComplete="email"
+                                    autoFocus
+                                    onChange={(e)=> setReferenceSite(e.target.value)}
+                                    />
+                                    <GenreCodeSelector setGenreCode={setGenreCode} />
+                                    <BudgetCodeSelector setBugetCode={setBugetCode} />
+                                    <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                    onClick={(e)=>clicked(e)}
+                                    disabled={!referenceSite}
+                                    >
+                                    検索
+                                    </Button>
+                                </>
+                                }
 
 
+                            </Box>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </ThemeProvider>
         </>
 
     )
