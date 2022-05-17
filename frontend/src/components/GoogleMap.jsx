a0cba15423bcf466d1bbeb1c73e83ef4675f2212
ouioui99 <qp5minute@gmail.com>
2022-05-17 11:57:43 +0900

    markerが置かれた際に地図のセンター1gaそのマーカーになるよう変更

--- frontend/src/components/GoogleMap.jsx
+++ frontend/src/components/GoogleMap.jsx
@@ -6,11 +6,6 @@ const containerStyle = {
   height: "100%",
 };
 
-const center = {
-  lat: 35.69575,
-  lng: 139.77521,
-};
-
 const GoogleMapsApiKey = process.env.React_APP_GOOGLE_MAP_API;
 
 export const GoogleMapComponent = (props) => {
@@ -19,12 +14,22 @@ export const GoogleMapComponent = (props) => {
   const resultLat = Number(props.resultSitePosition.lat);
   const resultlng = Number(props.resultSitePosition.lng);
 
+  const [centerPosition, setCenterPosition] = useState({
+    lat: 35.69575,
+    lng: 139.77521,
+  });
+
   const [referenceSiteMarkerPositions, setReferenceSiteMarkerPositions] =
     useState([]);
 
   const [resultSitePositions, setResultSitePositions] = useState([]);
 
+  const [test, setTest] = useState(false);
+
   useEffect(() => {
+    if (test) {
+      setCenterPosition({ lat: referenceSiteLat, lng: referenceSiteLng });
+    }
     //検索する基準地の座標
     setReferenceSiteMarkerPositions([
       ...referenceSiteMarkerPositions,
@@ -45,12 +50,17 @@ export const GoogleMapComponent = (props) => {
     //   );
     // }
     // console.log(referenceSiteMarkerPositions);
+    setTest(true);
   }, [props]);
 
   return (
     <>
       <LoadScript googleMapsApiKey={GoogleMapsApiKey}>
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={centerPosition}
+          zoom={10}
+        >
           {referenceSiteMarkerPositions.map((marker, index) => {
             //referenceSiteMarkerPositionsの一番新しいものだけをMarkerとして返却する
             return referenceSiteMarkerPositions.length == index + 1 ? (
