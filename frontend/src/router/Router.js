09510d74b247185fa0a3583a1827ada4fa878556
ouioui99 <qp5minute@gmail.com>
2022-05-24 13:37:56 +0900

    close #3 notFound遷移実装

--- frontend/src/router/Router.js
+++ frontend/src/router/Router.js
@@ -26,6 +26,7 @@ function Router() {
               </Auth>
             }
           />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </UserProvider>
     </BrowserRouter>
